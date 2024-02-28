0215797e9aedb8f760d75e608060fd71cb451f1f
Mohammad Amin <dadgaramin96@gmail.com>
2024-02-28 18:24:56 +0330

    update: removing the dataframe filtering! It wasn't useful for now as it could make the whole table to be reloaded.

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from utils.mongo import MongoSingleton
 from utils.process_guild_data import process_guild_data
 
 
-@st.cache_data
-def load_guilds_latest_date_df() -> list[dict[str, str | datetime]]:
+# @st.cache_data
+def load_guilds_latest_date_df():
     client = MongoSingleton.get_instance().client
 
     cursor = client["Core"]["platforms"].find({"name": "discord"})
@@ -38,75 +38,6 @@ def load_guilds_latest_date_df() -> list[dict[str, str | datetime]]:
 
         df = pd.DataFrame(guilds_data)
         dataframe_widget.dataframe(df)
-
-    return df
-
-
-def process_df(data: pd.DataFrame) -> pd.DataFrame:
-    """
-    add the filtering option to the dataframe
-    """
-    modify = st.checkbox("Add filters")
-    if not modify:
-        df = data.copy()
-    else:
-        df = data.copy()
-
-        # Try to convert datetimes into a standard format (datetime, no timezone)
-        for col in df.columns:
-            if is_object_dtype(df[col]):
-                try:
-                    df[col] = pd.to_datetime(df[col])
-                except Exception:
-                    pass
-
-            if is_datetime64_any_dtype(df[col]):
-                df[col] = df[col].dt.tz_localize(None)
-
-        modification_container = st.container()
-
-        with modification_container:
-            to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
-            for column in to_filter_columns:
-                left, right = st.columns((1, 20))
-                # Treat columns with < 10 unique values as categorical
-                if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-                    user_cat_input = right.multiselect(
-                        f"Values for {column}",
-                        df[column].unique(),
-                        default=list(df[column].unique()),
-                    )
-                    df = df[df[column].isin(user_cat_input)]
-                elif is_numeric_dtype(df[column]):
-                    _min = float(df[column].min())
-                    _max = float(df[column].max())
-                    step = (_max - _min) / 100
-                    user_num_input = right.slider(
-                        f"Values for {column}",
-                        min_value=_min,
-                        max_value=_max,
-                        value=(_min, _max),
-                        step=step,
-                    )
-                    df = df[df[column].between(*user_num_input)]
-                elif is_datetime64_any_dtype(df[column]):
-                    user_date_input = right.date_input(
-                        f"Values for {column}",
-                        value=(
-                            df[column].min(),
-                            df[column].max(),
-                        ),
-                    )
-                    if len(user_date_input) == 2:
-                        user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                        start_date, end_date = user_date_input
-                        df = df.loc[df[column].between(start_date, end_date)]
-                else:
-                    user_text_input = right.text_input(
-                        f"Substring or regex in {column}",
-                    )
-                    if user_text_input:
-                        df = df[df[column].astype(str).str.contains(user_text_input)]
 
 
 logging.basicConfig(level=logging.INFO)
